37b31a6e749a918cf60783071892e4207d5eacd5
Michael Fahy <fahy@chapman.edu>
2020-02-19 21:26:19 -0800

    updates for version 3.1

--- UdpClient.java
+++ UdpClient.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
 *  UDP Client Program
 *  Connects to a UDP Server
@@ -5,18 +11,12 @@
 *  Receives a response from the server and displays it.
 *
 *  @author: Michael Fahy
-*  email: fahy@chapman.edu
-*  date: 2/4/2018
-*  @version: 3.0
+*     email: fahy@chapman.edu
+*     date: 2/4/2019
+*  @version: 3.1
 *
 */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 class UdpClient {
   public static void main(String[] args) throws Exception {
@@ -29,6 +29,8 @@ class UdpClient {
 
     byte[] sendData = new byte[1024];
     byte[] receiveData = new byte[1024];
+
+    System.out.println("Type a Sentence");
 
     String sentence = inFromUser.readLine();
     sendData = sentence.getBytes();
